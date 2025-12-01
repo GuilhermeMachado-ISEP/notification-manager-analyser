c58b106e5d6f868d9d09a5d013d514dce98e9b56
Guilherme Machado-ISEP <1240713@isep.ipp.pt>
2025-12-01 23:54:18 +0000

    Creation of a non-graphical interface to interact and test interactions with the program(code): Further development for the required UI changes in order to test out the initial algorithm design [#7]

--- src/main/java/Interface/nonGraphicalUI.java
+++ src/main/java/Interface/nonGraphicalUI.java
@@ -1,15 +1,19 @@
 package Interface;
 
 import Domain.Option;
+import Domain.Utils;
 
-public class nonGraphicalUI extends Runnable{
+public class nonGraphicalUI implements Runnable{
     public void run(){
-
+        start();
     }
 
     public void start(){
-        System.out.println("############Non Graphical interface(Ideal for teses)#############");
-        Option[] options = {new Option(1,"Exit",null)};
+        System.out.println("############Non Graphical interface(Ideal for tests)#############");
+        System.out.println("Please choose an option:");
+        Runnable display = new notificationDisplayUI();
+        Option[] options = {new Option(1, "Display Notifications", display), new Option(2,"Exit",null)};
+        Utils.chooseOne(options).run();
     }
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,7 @@
-public class main {
+import Interface.nonGraphicalUI;
 
+public class main {
+    public static void main(String[] args) {
+        new nonGraphicalUI().start();
+    }
 }
